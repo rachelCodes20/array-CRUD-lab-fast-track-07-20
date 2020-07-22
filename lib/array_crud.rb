d773277d8e62340a9f7294f0590854f9c3086019
Rachel Collins <rachel.collins@my.liu.edu>
2020-07-22 03:23:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,34 +1,50 @@
 def create_an_empty_array
+   []
+end
+
+def create_an_array(create)
+create = [1,2,3,4]
+
+end
+create_an_array(create)
+
+def create_an_array(array_one)
+
+array_one = []
+
+array_one.push("monday", "tuesday", "wednesday", "thursday")
+end
+create_an_array(array_one.push)
+
+def add_element_to_end_of_array(array_one, element)
+  
+  element = "thursday"
+   array_one.unshift(element)  
   
 end
 
-def create_an_array
-  
-end
-
-def add_element_to_end_of_array(array, element)
-  
-end
-
-def add_element_to_start_of_array(array, element)
-  
-end
 
 def remove_element_from_end_of_array(array)
   
 end
 
 def remove_element_from_start_of_array(array)
+  new_array = array_one.pop([0])
   
 end
+
+remove_element_from_start_of_array(new_array)
 
 def retrieve_element_from_index(array, index_number)
   
 end
 
-def retrieve_first_element_from_array(array)
+def retrieve_first_element_from_array(array_one)
   
+  array_one[1]
 end
+retrieve_first_element_from_array(array_one[1])
+
 
 def retrieve_last_element_from_array(array)
   
